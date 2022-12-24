eee0ef7c5298b7f1845ab4afab24b913a1d6592f
Heba Hiari <hebahiary+github@gmail.com>
2022-12-24 09:19:49 -0800

    Longest Common Prefix

--- leetcode.js
+++ leetcode.js
@@ -112,3 +112,38 @@ var romanToInt = function (s) {
 
   return result;
 };
+
+//////////////////////////////
+
+// # 14: Longest Common Prefix
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// SOLUTION: (personal) :
+
+var longestCommonPrefix = function (strs) {
+  let longest = strs[0].split("");
+
+  for (let i = 1; i < strs.length; i++) {
+    for (let j = 0; j < longest.length; j++) {
+      if (longest[j] !== strs[i][j]) {
+        longest.splice(j);
+      }
+    }
+  }
+
+  return longest.join("");
+};
