86be85e78dfac89a8cb8aa536e5fe4e4fef10ff8
Heba Hiari <hebahiary+github@gmail.com>
2022-12-25 15:23:45 -0800

    Merge Two Sorted Lists

--- leetcode.js
+++ leetcode.js
@@ -1,4 +1,4 @@
-// #1 TWO SUM
+// #1 TWO SUM -- easy
 
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -40,7 +40,7 @@ var twoSum = (nums, target, map = new Map()) => {
 
 ///////////////////////////////
 
-// #13 Roman to Integer
+// #13 Roman to Integer -- easy
 
 // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
@@ -115,7 +115,7 @@ var romanToInt = function (s) {
 
 //////////////////////////////
 
-// # 14: Longest Common Prefix
+// # 14: Longest Common Prefix -- easy
 
 // Write a function to find the longest common prefix string amongst an array of strings.
 
@@ -150,7 +150,7 @@ var longestCommonPrefix = function (strs) {
 
 //////////////////////
 
-// #20: Valid Parentheses
+// #20: Valid Parentheses -- easy
 
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
@@ -204,4 +204,55 @@ var isValid = function (s) {
   }
 
   return true;
+};
+
+// 21. Merge Two Sorted Lists -- easy
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+// Example 1:
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+// SOLUTION: (neetcode):
+
+var mergeTwoLists = function (list1, list2) {
+  const isBaseCase1 = list1 === null;
+  if (isBaseCase1) return list2;
+
+  const isBaseCase2 = list2 === null;
+  if (isBaseCase2) return list1;
+
+  const isL2Greater = list1.val <= list2.val;
+  if (isL2Greater) {
+    list1.next = mergeTwoLists(
+      list1.next,
+      list2
+    ); /* Time O(N + M) | Space O(N + M) */
+
+    return list1;
+  }
+
+  const isL2Less = list2.val <= list1.val;
+  if (isL2Less) {
+    list2.next = mergeTwoLists(
+      list1,
+      list2.next
+    ); /* Time O(N + M) | Space O(N + M) */
+
+    return list2;
+  }
 };
