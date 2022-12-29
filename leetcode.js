779e5969b5f30a0d7431d5f3540e3d383958fc02
Heba Hiari <hebahiary+github@gmail.com>
2022-12-29 10:11:41 -0800

    added Merge Sorted Array

--- leetcode.js
+++ leetcode.js
@@ -408,3 +408,26 @@ var mySqrt = function (x) {
   }
   return left - 1;
 };
+
+// 88. Merge Sorted Array
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+//SOLUTION: (internet)
+
+// Two pointer solution
+// Start by comparing the largest numbers between the two arrays and add to the end of nums1
+var merge = function (nums1, m, nums2, n) {
+  while (n) {
+    if (nums1[m - 1] > nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[--m];
+    } else {
+      nums1[m + n - 1] = nums2[--n];
+    }
+  }
+  return nums1;
+};
