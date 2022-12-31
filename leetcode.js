ad9745ef40b47e01201c626d23e21341ed707ffc
Heba Hiari <hebahiary+github@gmail.com>
2022-12-30 21:09:27 -0800

    added Pascal's Triangle

--- leetcode.js
+++ leetcode.js
@@ -431,3 +431,45 @@ var merge = function (nums1, m, nums2, n) {
   }
   return nums1;
 };
+
+// 118. Pascal's Triangle
+
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Example 2:
+
+// Input: numRows = 1
+// Output: [[1]]
+
+//SOLUTION: (personal)
+
+var generate = function (numRows) {
+  let result = [];
+  let currentArray = [1, 1];
+
+  if (numRows === 0) return [];
+  if (numRows === 1) return [[1]];
+
+  for (let i = 2; i < numRows; i++) {
+    let subArray = [];
+
+    for (let j = 0; j < currentArray.length - 1; j++) {
+      subArray.push(currentArray[j] + currentArray[j + 1]);
+    }
+
+    currentArray = subArray;
+    subArray.push(1);
+    subArray.unshift(1);
+    result.push(subArray);
+  }
+
+  result.unshift([1, 1]);
+  result.unshift([1]);
+  return result;
+};
