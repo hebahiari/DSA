3f0ed0402c4789c00ee3d0e31140d1ecf5d74162
Heba Hiari <hebahiary+github@gmail.com>
2023-01-15 11:26:44 -0800

    added Happy Number

--- leetcode.js
+++ leetcode.js
@@ -889,3 +889,41 @@ var reverseBits = function (n) {
   }
   return result;
 };
+
+// 202. Happy Number
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+// Example 1:
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+// Example 2:
+
+// Input: n = 2
+// Output: false
+
+// SOLUTION: (personal)
+
+var isHappy = function (n, mySet = new Set()) {
+  let string = n.toString();
+  let added = 0;
+  for (let i = 0; i < string.length; i++) {
+    added += Math.pow(string[i], 2);
+  }
+  if (added == 1) return true;
+  if (mySet.has(added)) return false;
+  mySet.add(added);
+  return isHappy(added, mySet);
+};
