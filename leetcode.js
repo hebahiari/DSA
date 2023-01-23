ae3bb0da833591ed75d7aaa3b0c8311bed2c50ae
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2023-01-23 11:29:50 -0800

    added Convert Sorted Array to Binary Search Tree

--- leetcode.js
+++ leetcode.js
@@ -505,6 +505,45 @@ var isSymmetric = function (left_tree, right_tree = left_tree) {
 };
 
 
+// 108. Convert Sorted Array to Binary Search Tree
+
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a 
+// height-balanced
+//  binary search tree.
+
+ 
+
+// Example 1:
+
+
+// Input: nums = [-10,-3,0,5,9]
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+// Example 2:
+
+
+// Input: nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+// SOLUTION (personal):
+
+var sortedArrayToBST = function(nums) {
+  if(nums.length) {
+
+  if(nums.length == 1) {
+      return new TreeNode(nums[0])
+  }
+  let middle = Math.floor(nums.length/2)
+
+  return new TreeNode(nums[middle], sortedArrayToBST(nums.slice(0, middle)), sortedArrayToBST(nums.slice(middle+1, nums.length)))
+
+  } 
+
+  return null
+};
+
 // 118. Pascal's Triangle
 
 // Given an integer numRows, return the first numRows of Pascal's triangle.
@@ -1031,3 +1070,5 @@ var reverseList = function (head) {
 
   return currentNode;
 };
+
+
